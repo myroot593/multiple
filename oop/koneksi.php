824081f01a4cd3855398dc722999ba9a1b314feb
Ahmad Zaelani <myroot593@gmail.com>
2020-09-30 15:48:52 +0700

    Create koneksi.php

---
+++ oop/koneksi.php
@@ -0,0 +1,18 @@
+<?php
+class Database{
+	private $host="localhost";
+	private $user="root";
+	private $pass="";
+	private $db="codexam";
+	public $koneksi;
+	function __construct(){
+		$this->koneksi = new mysqli($this->host, $this->user, $this->pass, $this->db);
+		if($this->koneksi==false):
+			die("Error database connection :".$this->koneksi->connect_error());
+		endif;
+
+		return $this->koneksi;
+	}
+}
+
+?>
